2e032e30efaabbbdbb99c1fde5f05a895a1cd852
Michal Franc <lam.michal.franc@gmail.com>
2012-07-24 23:25:40 +0200

    - refactored NLogLogger.cs

--- src/LearnDash/Core/Extensions/NLogLogger.cs
+++ src/LearnDash/Core/Extensions/NLogLogger.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Core.Extensions
+﻿namespace Core.Extensions
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
     using NLog;
 
     public static class NLogLogger
@@ -12,8 +12,11 @@ namespace Core.Extensions
         public static string ExtractInnerException(Exception ex)
         {
             var innerException = string.Empty;
+
             if (ex.InnerException != null)
+            {
                 innerException = ExtractInnerException(ex.InnerException);
+            }
 
             return string.Format("{0} \r\n   {1}", ex.Message, innerException);
         }
@@ -21,8 +24,11 @@ namespace Core.Extensions
         public static void ErrorExceptionsWithInner(this Logger logger,string message,Exception ex)
         {
             var innerExceptions = string.Empty;
-            if(ex.InnerException != null)
+
+            if (ex.InnerException != null)
+            {
                 innerExceptions = ExtractInnerException(ex.InnerException);
+            }
 
             logger.Error(string.Format("{0} Exception\r\n {1} \r\n--------------\r\n {2}", message, ex.Message, innerExceptions));
         }
