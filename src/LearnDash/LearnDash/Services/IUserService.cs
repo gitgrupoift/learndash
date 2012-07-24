9c2b2f255b47f923c0127ce68e89a4626d79458e
Michal Franc <lam.michal.franc@gmail.com>
2012-07-24 23:15:53 +0200

    - IUserService files

---
+++ src/LearnDash/LearnDash/Services/IUserService.cs
@@ -0,0 +1,27 @@
+﻿namespace LearnDash.Services
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using LearnDash.Dal;
+    using LearnDash.Dal.Models;
+    using LearnDash.Dal.NHibernate;
+
+    public interface IUserService
+    {
+        //UserProfile GetCurrentUser();
+    }
+
+    public class UserService : IUserService
+    {
+        private readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public IRepository<UserProfile> UserRepo { get; set; }
+
+        //public UserProfile GetCurrentUser()
+        //{
+        //    UserRepo.GetByParameterEqualsFilter("UserId", User.Identity.Name).SingleOrDefault();
+        //    throw new System.NotImplementedException();
+        //}
+    }
+}
