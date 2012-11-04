088a1cf18dd10d3395ceb1e610f648892bbeab0a
LaMik <lam.michal.franc@gmail.com>
2012-11-04 10:36:17 +0100

    moved json extensions from controller

--- src/LearnDash/LearnDash/Controllers/LearningFlowController.cs
+++ src/LearnDash/LearnDash/Controllers/LearningFlowController.cs
@@ -336,39 +336,4 @@
             }
         }
     }
-
-    public static class Is
-    {
-        public class Success
-        {
-            public static object Empty
-            {
-                get
-                {
-                    return new { isSuccess = true, message = string.Empty };
-                }
-            }
-
-            public static object Message(string message)
-            {
-                return new
-                {
-                    isSuccess = true,
-                    message
-                };
-            }
-        }
-
-        public class Fail
-        {
-            public static object Message(string message)
-            {
-                return new
-                           {
-                               isSuccess = false,
-                               message
-                           };
-            }
-        }
-    }
 }
